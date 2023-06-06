2e353700b5b208b76dda04846f74e5c3af1282a7
Cordet <cgula@pdx.edu>
2023-06-06 16:37:08 -0700

    Add favicon images and 'logo'

---
+++ scripts/profolio-home.js
@@ -0,0 +1,19 @@
+// Print to console for loaded page elements
+window.addEventListener('load', loadPage);
+
+// Load the page and print info to the console
+function loadPage(event) {
+	const main = document.querySelector('main');
+	main.appendChild(homeMessage());
+}
+
+homeMessage = () => {
+	const message = document.createElement('p');
+	message.setAttribute('class', 'homeMessage');
+	message.textContent = `Welcome [Καλώς ήρθες] to my professional portfolio! \n
+	You are currently on the 'Home' page. Feel free to checkout the 'About Me' 
+	page to learn more about me, the 'Experience' page to view my résumé, or the 
+	'Projects' page to view my project showcase. \n
+	To leave your contact info, visit 'Contact' and fill out the form provided. `;
+	return message;
+};
